969fed87791927da1992a5027828a9714977b680
techied <11001514+techied@users.noreply.github.com>
2021-12-09 20:04:39 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-if [[ $DOWNLOADED ]]; then
-  echo "Download complete";
-
-  exit 0;
+if [[ -v DOWNLOADED ]]; then
+  if [[ $DOWNLOADED == 0 ]]; then
+    echo "Download complete"
+    exit 0
+  fi
 fi
+
+mkdir bandwidth-server
+
+cd bandwidth-server || exit
 
 wget https://dl.techied.me/latest.tar.gz
 
 tar -xvzf latest.tar.gz
-
-cd latest || exit
 
 chmod +x install.sh
 
