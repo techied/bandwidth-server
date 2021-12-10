04d043160582c5b7694b3f92def63e5a99080a94
techied <11001514+techied@users.noreply.github.com>
2021-12-09 20:29:30 -0500

    nvm install instead of use

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 if [[ -v DOWNLOADED ]]; then
   if [[ $DOWNLOADED == 0 ]]; then
     echo "Download complete"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+    source ~/.bashrc
+    nvm install 16
     exit 0
   fi
 fi
