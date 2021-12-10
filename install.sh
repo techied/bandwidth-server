9bb239989d148d93d419541d49e02d2188d7a05e
techied <11001514+techied@users.noreply.github.com>
2021-12-09 20:57:34 -0500

    fix mongo installation

--- install.sh
+++ install.sh
@@ -15,7 +15,10 @@ if [[ -v DOWNLOADED ]]; then
     case $MONGODB_INSTALLATION in
     1)
       echo "Installing MongoDB locally"
-      apt install -y mongodb-org
+      wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+      echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+      sudo apt update
+      sudo apt install -y mongodb-org
       systemctl start mongod
       systemctl enable mongod
       echo "MONGODB_URI=mongodb://localhost:27017/main" | tee .env
